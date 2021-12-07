7909a7c6d483e560d2ef65beb7bbb25c40ef4d94
Lyz Coote <44366896+lyzcoote@users.noreply.github.com>
2021-12-07 18:20:04 +0100

    Update api.py

--- api.py
+++ api.py
@@ -17,12 +17,14 @@ result = requests.get(url, headers=headers)
 print(result.status_code)
 print(result.json())
 
+"""
 url = 'https://api.vrchat.cloud/api/1/users/lyzcoote/name'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
  'Cookie': 'apiKey=' + apiKey}
 result = requests.get(url, headers=headers)
 print(result.status_code)
 print(result.json())
+"""
 
 """Make a custom API GET Request with a custom header and basic HTTP authentication"""
 url = 'https://api.vrchat.cloud/api/1/auth'
@@ -44,7 +46,7 @@ print(result.content)
 print("API Key: "+ str(apiKey))
 url = 'https://api.vrchat.cloud/api/1/users/lyzcoote/name'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
-    'Cookie': 'apiKey=' + apiKey}
+    'Cookie': 'apiKey=' + apiKey, 'Cookie': 'auth=' + authToken}
 result = requests.get(url, headers=headers)
 print("Status Code: "+ str(result.status_code))
 print(result.content)
