3544fa0992c515eac9c08fc52a18245bef6bba46
Lyz Coote <44366896+lyzcoote@users.noreply.github.com>
2021-12-07 18:14:37 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,15 +1,50 @@
-"""Do a API request to a URL given by the user and print the response."""
-def main():
-    """Main function."""
-    import json
-    import requests as reqs
+import requests
+from requests import api
+url = 'https://api.vrchat.cloud/api/1/config'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+result = requests.get(url, headers=headers)
+"""Prints the result of the request with the status code and formatted JSON"""
+print(result.status_code)
+print(result.json())
+"""Extract the value called apiKey from the JSON and print it"""
+apiKey = result.json()['apiKey']
 
-    # Make the HTTP request.
-    response = reqs.get('https://vrchat.com/api/1/users/lyzcoote/name')
+"""Make a custom API GET Request with a custom header"""
+url = 'https://api.vrchat.cloud/api/1/auth/exists?username=lyzcoote'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+ 'Cookie': 'apiKey=' + apiKey}
+result = requests.get(url, headers=headers)
+print(result.status_code)
+print(result.json())
 
-    # Use the json module to load CKAN's response into a dictionary.
-    response_dict = json.loads(response.text)
+url = 'https://api.vrchat.cloud/api/1/users/lyzcoote/name'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+ 'Cookie': 'apiKey=' + apiKey}
+result = requests.get(url, headers=headers)
+print(result.status_code)
+print(result.json())
 
-    for i in response_dict:
-        print("key: ", i, "val: ", response_dict[i])
+"""Make a custom API GET Request with a custom header and basic HTTP authentication"""
+url = 'https://api.vrchat.cloud/api/1/auth'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+    'Cookie': 'apiKey=' + apiKey}
+result = requests.get(url, headers=headers, auth=('ExtremistShip', '67PxFs5ls1'))
+print(result.status_code)
+print(result.content)
+authToken = result.json()['token']
+print(authToken)
 
+url = 'https://api.vrchat.cloud/api/1/auth/user'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+    'Cookie': 'auth=' + authToken}
+result = requests.get(url, headers=headers, auth=('ExtremistShip', '67PxFs5ls1'))
+print(result.status_code)
+print(result.content)
+
+print("API Key: "+ str(apiKey))
+url = 'https://api.vrchat.cloud/api/1/users/lyzcoote/name'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+    'Cookie': 'apiKey=' + apiKey}
+result = requests.get(url, headers=headers)
+print("Status Code: "+ str(result.status_code))
+print(result.content)
