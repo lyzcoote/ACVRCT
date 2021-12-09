827e6d516a6d3c736af0cb867a05e9fdfa41ad11
Lyz Coote <44366896+lyzcoote@users.noreply.github.com>
2021-12-09 21:25:55 +0100

    Ver: 0.2.1.1Alpha
    Organized all the functions in their specific files
    Trying to make a more cleaner code

---
+++ release/logManager.py
@@ -0,0 +1,20 @@
+################################################################################
+#                                                                              #
+#                                   Functions                                  #
+#                                                                              #
+################################################################################
+
+def logger(message, message_type):
+    """
+    Return a message with a message type and time
+    """
+    from datetime import datetime
+    now = datetime.now()
+
+    current_time = now.strftime("%H:%M:%S")
+    if message_type == "error":
+        print("[{}] [ERROR]: {}".format(current_time, message))
+    elif message_type == "warning":
+        print("[{}] [WARNING]: {}".format(current_time, message))
+    elif message_type == "success":
+        print("[{}] [SUCCESS]: {}".format(current_time, message))
